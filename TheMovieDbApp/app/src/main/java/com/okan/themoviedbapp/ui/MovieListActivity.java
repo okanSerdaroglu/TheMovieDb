702850149976801c18a9d57362ad0c1452bfcb0d
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-07-12 18:14:13 +0300

    MoviesListActivity

---
+++ TheMovieDbApp/app/src/main/java/com/okan/themoviedbapp/ui/MovieListActivity.java
@@ -0,0 +1,37 @@
+package com.okan.themoviedbapp.ui;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.okan.themoviedbapp.R;
+
+public class MovieListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_list);
+
+        setSupportActionBar(findViewById(R.id.toolbar));
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+}
